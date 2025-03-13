c1fd0a17088767e69c6fb845bdc3332502e56f53
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-03-14 03:33:16 +0530

    For Loop code added

--- Loops/for_loop.dart
+++ Loops/for_loop.dart
@@ -1,1 +1,16 @@
-void main() {}
+void main() {
+  // FOR Loop
+
+  // WAP to find the even numbers between 1 to 20
+  for (var i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
+
+  // for ..in loop
+  List planetList = ["Mercury", "Mars", "Earth", "Venus"];
+  for (String planet in planetList) {
+    print(planet);
+  }
+}
