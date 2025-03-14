c90cf2a0716145ebd890952a9194d9b0c4459b5b
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-03-14 15:06:44 +0530

    While Loop added

---
+++ Loops/while_loop.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  // WHILE Loop
+  // WAP to print the even numbers between 1 t 10
+  var i = 1;
+  while (i <= 10) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+    i++;
+  }
+}
