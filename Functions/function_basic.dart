7051c9943948d047778b59b45b9d933cf2163a20
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-03-14 16:50:42 +0530

    Some changes in function and fatArrow code added

--- Functions/function_basic.dart
+++ Functions/function_basic.dart
@@ -1,7 +1,11 @@
 void main(List<String> args) {
   findPerimeter(2, 8);
+  squareArea(4);
   int recArea = getArea(4, 2);
   print("The rectangle area is : $recArea");
+
+  double triArea = triangleArea(3, 8);
+  print("The triangle area is : $triArea");
 }
 
 // This function is not returning any value so no retrun type only void
@@ -17,3 +21,9 @@ int getArea(int length, int breadth) {
   int area = length * breadth;
   return area;
 }
+
+// Expression in Function : SHORT HAND SYNTAX
+
+void squareArea(int side) => print("The area of square is : ${side * side}");
+
+double triangleArea(int base, int height) => (base * height) / 2;
