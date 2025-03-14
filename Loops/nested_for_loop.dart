9fe944ccb67f1ab35b3ccfc3ba73c26f5ec0a297
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-03-14 15:44:12 +0530

    Nested For Loop with Break and Continue Kerword and Labels Added

--- Loops/nested_for_loop.dart
+++ Loops/nested_for_loop.dart
@@ -1,8 +1,6 @@
 void main(List<String> args) {
-  // Nested FOR Loop
-  // BREAK Keyword
-  // LABELS
-
+// BREAK Keyword
+  print("BREAK Keyword Output:");
   for (var i = 1; i <= 10; i++) {
     print(i);
     if (i == 5) {
@@ -10,7 +8,8 @@ void main(List<String> args) {
     }
   }
 
-  print("Nested Loop Start From Here:");
+// Nested FOR Loop
+  print("Nested Loop With Break Keyword Output:");
   for (var i = 1; i <= 3; i++) {
     for (var j = 1; j <= 3; j++) {
       print("$i $j");
@@ -20,7 +19,8 @@ void main(List<String> args) {
     }
   }
 
-  print("Labels Code Start From Here:");
+// LABELS in Nested FOR Loop With BREAK Keyword
+  print("Labels in Nested FOR Loop With Break Keyword Output :");
   outerLoop:
   for (var i = 1; i <= 3; i++) {
     innerLoop:
@@ -37,4 +37,38 @@ void main(List<String> args) {
 
   // Do some testing by changing the place of labels you will get to know
   // something new.
+
+  // CONTINUE Keyword
+  print("CONTINUE Keyword Output:");
+  for (var i = 1; i <= 10; i++) {
+    if (i == 4) {
+      continue;
+    }
+    print(i);
+  }
+
+  // WAP to print ODD numbers between 1 to 10 using CONTINUE Keyword
+  print("ODD Numbers Using CONTINUE Keyword Output:");
+  for (var i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      continue;
+    }
+    print(i);
+  }
+
+// LABELS in Nested FOR Loop With CONTINUE Keyword
+  print("LABELS in Nested FOR Loop With CONTINUE Keyword Output:");
+  outerLoop:
+  for (var i = 1; i <= 3; i++) {
+    innerLoop:
+    for (var j = 1; j <= 3; j++) {
+      if (i == 2 && j == 2) {
+        continue outerLoop;
+      }
+      if (i == 3 && j == 1) {
+        continue innerLoop;
+      }
+      print("$i $j");
+    }
+  }
 }
