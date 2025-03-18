d649e50f9f8947945a3a2beaea28ce5c6a20d114
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-03-19 02:19:57 +0530

    Named Constructor code added

--- Class/constructor.dart
+++ Class/constructor.dart
@@ -18,6 +18,18 @@ void main() {
       "Second student id : ${student2.id} and Second student name : ${student2.name}");
   student2.study();
   student2.sleep();
+  print("");
+
+  var student3 = Student.myCustomConstructor();
+  student3.id = 50;
+  student3.name = "Paul";
+  print(
+      "Third student id : ${student3.id} and Third student name : ${student3.name}");
+  print("");
+
+  var student4 = Student.myAnotherCustomCunstructor(678, "Samiul");
+  print(
+      "Fourth student id : ${student4.id} and Fourth student name : ${student4.name}");
 }
 
 // Define states (properties) and behaviour of a student
@@ -25,7 +37,14 @@ class Student {
   int id = 1; // Instance or Field variable, default value null
   String name = "Sam"; // Instance or Field variable, default value null
 
-  Student(this.id, this.name);
+  Student(this.id, this.name); // This is the constructor of the class Student
+
+  Student.myCustomConstructor() {
+    // Named constructor
+    print("This is my custom cunstructor");
+  }
+
+  Student.myAnotherCustomCunstructor(this.id, this.name); // Named constructor
 
   void study() {
     print("${this.name} is now studying!");
